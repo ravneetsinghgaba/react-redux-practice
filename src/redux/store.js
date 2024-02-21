6eb9d18a186202daa52eef0f06bae5121aace5d1
Ravneet Singh <ravneetsingh.gaba@gmail.com>
2024-02-22 01:53:50 +0530

    whole code, css is a bit messed

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { productReducer, selectedProductReducer } from './reducers/productReducer';
+
+
+const store = configureStore({
+    reducer: {
+        allProducts: productReducer,
+        product: selectedProductReducer
+    }
+})
+
+export default store;
